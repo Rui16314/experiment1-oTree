3a6503632b3fc502261e4d34643c29d4dec91353
Rui16314 <rui315@yorku.ca>
2025-08-21 13:22:44 -0400

    Update tests.py

--- experiment1/tests.py
+++ experiment1/tests.py
@@ -1,1 +1,13 @@
-from otree.api import *
+from otree.api import Bot, currency_range, Currency as cu
+from .models import C
+
+
+class PlayerBot(Bot):
+    def play_round(self):
+        # See your valuation; bid a fraction of it.
+        val = self.player.valuation or cu(0)
+        my_bid = round(float(val) * 0.7, 2)
+        yield self.pages.Instructions
+        yield self.pages.Bid, dict(bid=cu(my_bid))
+        yield self.pages.Results
+
