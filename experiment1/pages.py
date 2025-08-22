0590ae737aba3ea9c57b1726a23bb453cb3eb777
Rui16314 <rui315@yorku.ca>
2025-08-22 18:07:30 -0400

    Update pages.py

--- experiment1/pages.py
+++ experiment1/pages.py
@@ -1,13 +1,11 @@
 # experiment1/pages.py
 from otree.api import *
-from otree.api import Currency as cu
 from .models import C, Player, Group, Subsession, set_payoffs, draw_valuation
 
 class Instructions(Page):
     def vars_for_template(self):
-        s = self.session
         return dict(
-            price_rule='first-price',      # keep simple for now
+            price_rule='first-price',
             matching='random',
             tie_rule='random',
             chat=False,
@@ -21,7 +19,6 @@ class Bid(Page):
     timeout_submission = {'bid': cu(0)}
 
     def vars_for_template(self):
-        # Safety: ensure valuation exists
         if self.player.valuation is None:
             self.player.valuation = draw_valuation()
         return dict(valuation=self.player.valuation)
@@ -30,9 +27,6 @@ class ResultsWaitPage(WaitPage):
     after_all_players_arrive = set_payoffs
 
 class Results(Page):
-    def is_displayed(self):
-        return True  # or: return self.group.price is not None
-
     def vars_for_template(self):
         opp = self.player.get_others_in_group()[0]
         you_won = (self.group.winner_id_in_group == self.player.id_in_group)
