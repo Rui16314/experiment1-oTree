7172adbeb8c6d4eb56247a01fcca350e646e9add
Rui16314 <rui315@yorku.ca>
2025-08-23 12:36:46 -0400

    Update pages.py

--- experiment1/pages.py
+++ experiment1/pages.py
@@ -1,37 +1,27 @@
 # experiment1/pages.py
 from otree.api import *
-from .models import (
-    C, Player, Group, Subsession,
-    set_payoffs, draw_valuation,
-    rules_for_round, phase_and_round_in_session
-)
+from .models import C, Player, Group, Subsession, set_payoffs, draw_valuation
 
 ROUNDS_PER_SESSION = 10
 
-
 def common_ctx(player: Player):
-    s, r = phase_and_round_in_session(player.round_number)
-    rule = rules_for_round(player.round_number)
-    price_label = 'first-price' if rule['price'] == 'first' else 'second-price'
+    r = player.round_number
     return dict(
-        session_no=s,
-        round_in_session=r,
-        price_rule=price_label,
-        matching=rule['matching'],
-        chat=rule['chat'],
-        ROUNDS_PER_SESSION=ROUNDS_PER_SESSION,
+        ROUNDS=ROUNDS_PER_SESSION,
+        session_num=((r - 1) // ROUNDS_PER_SESSION) + 1,
+        round_in_session=((r - 1) % ROUNDS_PER_SESSION) + 1,
     )
 
-
 class Instructions(Page):
-    # show at the start of each 10-round block
-    def is_displayed(self):
-        _, r = phase_and_round_in_session(self.round_number)
-        return r == 1
-
     def vars_for_template(self):
-        return common_ctx(self.player)
-
+        ctx = common_ctx(self.player)
+        ctx.update(dict(
+            price_rule='first-price',
+            matching='random',
+            tie_rule='random',
+            chat=False,
+        ))
+        return ctx
 
 class Bid(Page):
     form_model = 'player'
@@ -43,27 +33,26 @@ class Bid(Page):
         if self.player.valuation is None:
             self.player.valuation = draw_valuation()
         ctx = common_ctx(self.player)
-        ctx.update(valuation=self.player.valuation)
+        ctx.update(dict(valuation=self.player.valuation))
         return ctx
-
 
 class ResultsWaitPage(WaitPage):
     after_all_players_arrive = set_payoffs
-
 
 class Results(Page):
     def vars_for_template(self):
         ctx = common_ctx(self.player)
         opp = self.player.get_others_in_group()[0]
-        ctx.update(
+        you_won = (self.group.winner_id_in_group == self.player.id_in_group)
+        ctx.update(dict(
             your_bid=self.player.bid,
             opp_bid=opp.bid,
             valuation=self.player.valuation,
             price=self.group.price,
-            you_won=(self.group.winner_id_in_group == self.player.id_in_group),
-        )
+            you_won=you_won,
+        ))
         return ctx
 
-
 page_sequence = [Instructions, Bid, ResultsWaitPage, Results]
+
 
