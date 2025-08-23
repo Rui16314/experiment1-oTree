47e17c28bdadf6c2eec2e29c754326ad0f04f470
Rui16314 <rui315@yorku.ca>
2025-08-23 11:50:14 -0400

    Update pages.py

--- experiment1/pages.py
+++ experiment1/pages.py
@@ -1,41 +1,36 @@
 # experiment1/pages.py
 from otree.api import *
-from .models import C, set_payoffs, draw_valuation
+from .models import (
+    C, Player, Group, Subsession,
+    set_payoffs, draw_valuation,
+    rules_for_round, phase_and_round_in_session
+)
+
+ROUNDS_PER_SESSION = 10
+
+
+def common_ctx(player: Player):
+    s, r = phase_and_round_in_session(player.round_number)
+    rule = rules_for_round(player.round_number)
+    price_label = 'first-price' if rule['price'] == 'first' else 'second-price'
+    return dict(
+        session_no=s,
+        round_in_session=r,
+        price_rule=price_label,
+        matching=rule['matching'],
+        chat=rule['chat'],
+        ROUNDS_PER_SESSION=ROUNDS_PER_SESSION,
+    )
+
 
 class Instructions(Page):
+    # show at the start of each 10-round block
+    def is_displayed(self):
+        _, r = phase_and_round_in_session(self.round_number)
+        return r == 1
+
     def vars_for_template(self):
-        # For now you hard-code these (or read from session.config)
-        price_rule = 'first-price'   # 'first-price' or 'second-price'
-        matching   = 'random'        # 'random' or 'fixed'
-        chat       = False
-
-        # choose the partial to include
-        if price_rule == 'first-price':
-            if matching == 'random' and not chat:
-                tmpl = "experiment1/Instructions_S1_First.html"
-            elif matching == 'fixed' and not chat:
-                tmpl = "experiment1/Instructions_S2_FirstRepeated.html"
-            elif matching == 'fixed' and chat:
-                tmpl = "experiment1/Instructions_S3_FirstRepeated_Chat.html"
-            else:
-                tmpl = "experiment1/Instructions_S1_First.html"
-        else:
-            if matching == 'random' and not chat:
-                tmpl = "experiment1/Instructions_S4_Second.html"
-            elif matching == 'fixed' and not chat:
-                tmpl = "experiment1/Instructions_S5_SecondRepeated.html"
-            elif matching == 'fixed' and chat:
-                tmpl = "experiment1/Instructions_S6_SecondRepeated_Chat.html"
-            else:
-                tmpl = "experiment1/Instructions_S4_Second.html"
-
-        return dict(
-            price_rule=price_rule,
-            matching=matching,
-            chat=chat,
-            ROUNDS=C.NUM_ROUNDS,
-            instruction_tmpl=tmpl,
-        )
+        return common_ctx(self.player)
 
 
 class Bid(Page):
@@ -45,13 +40,11 @@ class Bid(Page):
     timeout_submission = {'bid': cu(0)}
 
     def vars_for_template(self):
-        # SAFE read that doesn't crash if the field is None
-        val = self.player.field_maybe_none('valuation')
-        if val is None:
-            # defensive fallback in case creating_session didn't run
-            val = draw_valuation()
-            self.player.valuation = val
-        return dict(valuation=val)
+        if self.player.valuation is None:
+            self.player.valuation = draw_valuation()
+        ctx = common_ctx(self.player)
+        ctx.update(valuation=self.player.valuation)
+        return ctx
 
 
 class ResultsWaitPage(WaitPage):
@@ -60,17 +53,17 @@ class ResultsWaitPage(WaitPage):
 
 class Results(Page):
     def vars_for_template(self):
+        ctx = common_ctx(self.player)
         opp = self.player.get_others_in_group()[0]
-        you_won = (self.group.winner_id_in_group == self.player.id_in_group)
-        return dict(
+        ctx.update(
             your_bid=self.player.bid,
             opp_bid=opp.bid,
             valuation=self.player.valuation,
             price=self.group.price,
-            you_won=you_won,
+            you_won=(self.group.winner_id_in_group == self.player.id_in_group),
         )
+        return ctx
 
 
 page_sequence = [Instructions, Bid, ResultsWaitPage, Results]
-
 
