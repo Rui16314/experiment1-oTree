86d9035a21b9e00425d21c1249f0a69d7c9c24e4
Rui16314 <rui315@yorku.ca>
2025-08-24 20:03:23 -0400

    Update pages.py

--- experiment1/pages.py
+++ experiment1/pages.py
@@ -1,25 +1,20 @@
 # experiment1/pages.py
 from otree.api import *
-from .models import (
-    C,
-    rules_for_round,
-    phase_and_round_in_session,
-    set_payoffs,
-    draw_valuation,
-)
+from .models import C, set_payoffs, session_and_within_round, rules_for_round
 
-def price_label_for_round(rn: int) -> str:
-    return 'second-price' if rules_for_round(rn)['price'] == 'second' else 'first-price'
+
+def price_label_for_round(rn: int):
+    return 'first-price' if rules_for_round(rn)['price'] == 'first' else 'second-price'
 
 
 class Instructions(Page):
     def vars_for_template(self):
-        s_no, r_in_s = phase_and_round_in_session(self.round_number)
+        s_no, r_in = session_and_within_round(self.round_number)
         r = rules_for_round(self.round_number)
         return dict(
-            ROUNDS_PER_SESSION=10,
+            ROUNDS_PER_SESSION=C.ROUNDS_PER_SESSION,
             session_no=s_no,
-            round_in_session=r_in_s,
+            round_in_session=r_in,
             price_rule=price_label_for_round(self.round_number),
             matching=r['matching'],
             tie_rule='random',
@@ -33,18 +28,13 @@ class Bid(Page):
     timeout_seconds = 60
     timeout_submission = {'bid': cu(0)}
 
-    def _ensure_valuation(self):
-        if self.player.valuation is None:
-            self.player.valuation = draw_valuation()
-        return self.player.valuation
-
     def vars_for_template(self):
-        s_no, r_in_s = phase_and_round_in_session(self.round_number)
+        s_no, r_in = session_and_within_round(self.round_number)
         return dict(
-            valuation=self._ensure_valuation(),
+            ROUNDS_PER_SESSION=C.ROUNDS_PER_SESSION,
             session_no=s_no,
-            round_in_session=r_in_s,
-            ROUNDS_PER_SESSION=10,
+            round_in_session=r_in,
+            valuation=self.player.valuation,
         )
 
 
@@ -54,20 +44,21 @@ class ResultsWaitPage(WaitPage):
 
 class Results(Page):
     def vars_for_template(self):
+        s_no, r_in = session_and_within_round(self.round_number)
         opp = self.player.get_others_in_group()[0]
         you_won = (self.group.winner_id_in_group == self.player.id_in_group)
-        s_no, r_in_s = phase_and_round_in_session(self.round_number)
         return dict(
+            ROUNDS_PER_SESSION=C.ROUNDS_PER_SESSION,
+            session_no=s_no,
+            round_in_session=r_in,
             your_bid=self.player.bid,
             opp_bid=opp.bid,
             valuation=self.player.valuation,
             price=self.group.price,
             you_won=you_won,
-            session_no=s_no,
-            round_in_session=r_in_s,
-            ROUNDS_PER_SESSION=10,
         )
 
 
 page_sequence = [Instructions, Bid, ResultsWaitPage, Results]
+
 
